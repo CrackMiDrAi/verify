2995ce4602377176d204727d6b3f208e8a5c33dc
Misa Liu <misaliu@misaliu.top>
2024-01-22 01:23:49 +0800

    refactor(client/captcha): Change the way to create a new CAPTCHA element

--- src/client/captcha.js
+++ src/client/captcha.js
@@ -1,20 +1,47 @@
 
-export function CreateNewCaptcha(element) {
+export function CreateNewCaptcha() {
+  const captchaOverlay = document.createElement('div');
+  const captchaContainer = document.createElement('div');
+
+  captchaOverlay.className = 'captcha-overlay';
+  captchaContainer.className = 'captcha-container';
+
+  captchaContainer.innerText = 'Loading reCAPTCHA...';
+
+  document.body.appendChild(captchaOverlay);
+  document.body.appendChild(captchaContainer);
+
+  // For some stupid animation
+  setTimeout(() => {
+    captchaOverlay.classList.add('show');
+    captchaContainer.classList.add('show');
+  }, 100);
+
+  const destroyCaptchaElement = () => {
+    setTimeout(() => {
+      captchaOverlay.classList.remove('show');
+      captchaContainer.classList.remove('show');
+    }, 500);
+
+    setTimeout(() => {
+      document.body.removeChild(captchaOverlay);
+      document.body.removeChild(captchaContainer);
+    }, 1000);
+  }
+
   return new Promise(async (res, rej) => {
-    const initCaptcha = () => {
-      grecaptcha.render(element, {
-        sitekey: import.meta.env.RECAPTCHA_SITEKEY,
-        callback: response => {
-          res(response);
-        },
-        'error-callback': e => {
-          rej(e);
-        },
-        'expired-callback': e => {
-          rej(e);
-        }
-      });
-    };
+    const initCaptcha = () => grecaptcha.render(captchaContainer, {
+      sitekey: import.meta.env.RECAPTCHA_SITEKEY,
+      theme: 'dark',
+      callback: response => {
+        res(response);
+        destroyCaptchaElement();
+      },
+      'error-callback': () => {
+        rej('Failed to load reCAPTCHA');
+        destroyCaptchaElement();
+      }
+    });
 
     let captchaLoadTimeCount = 0;
     const captchaApiClock = setInterval(() => {
